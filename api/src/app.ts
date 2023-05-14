ee3ac44ce658fc60fbe4dd38229bd6cec05259c4
Takayasu Beharu <info@jumpeer.jp>
2023-05-14 16:12:48 +0900

    [DONE] Update task status done

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,23 +3,58 @@ import cors from "cors"
 import { databaseManager } from "@/db/database_manager";
 
 export const app = express();
-// const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/tasks', async (req, res) => {
-
-  console.log("Request GET /api");
+app.get('/api/tasks', async (req, res) =>
+{
+  console.log("Request GET /api/tasks");
 
   const db = await databaseManager.getInstance();
   const result = await db.all("SELECT * FROM tasks");
-
-  // res.header('Content-Type', 'application/json; charset=utf-8');
   res.status(200).json(result);
-
-  // console.dir(res.status);
 });
 
-// app.use('/api', apiRouter);
+app.post('/api/tasks/register', async (req, res) =>
+{
+  console.log("Request POST /api/tasks/register");
+
+  const title = req.body.title;
+  const db = await databaseManager.getInstance();
+
+  console.log(`New task: ${title} ...`);
+
+  try {
+    await db.all(`INSERT INTO tasks (title, status) VALUES ('${title}', 0)`);
+    res.status(200).json({ message: `New task: ${title} registered` });
+    console.log("Succeeded");
+  } catch (e) {
+    res.status(500).json(e);
+    console.log("Failed");
+  }
+});
+
+app.post('/api/tasks/update', async (req, res) =>
+{
+  console.log("Request POST /api/tasks/update");
+
+  const task = { ...req.body };
+  // const id = req.body.id;
+  // const title =
+  // const title = req.body.status;
+
+  const db = await databaseManager.getInstance();
+
+  console.log(`Task Update: ${task.title} -> ${task.status == 1 ? "DONE" : "TO DO"}`);
+
+  try {
+    await db.all(`UPDATE tasks SET status=${task.status} WHERE id=${task.id}`);
+    res.status(200).json({ message: `Task Update: ${task.title} -> ${task.status == 1 ? "DONE" : "TO DO"}` });
+    console.log("Succeeded");
+  } catch (e) {
+    res.status(500).json(e);
+    console.log("Failed");
+  }
+});
--- front/src/ts/application.ts
+++ front/src/ts/application.ts
@@ -11,21 +11,31 @@ enum TaskStatus {
   DONE = 1,
 }
 
+type Elements = {
+  taskLists: HTMLElement | null;
+  taskRegisterTitle: HTMLInputElement | null;
+  taskRegister: HTMLElement | null;
+}
+
 export class Application {
 
   private tasks: Array<Task> = [];
-  private elems_taskLists: HTMLElement | null = null;
+  private elements: Elements = {} as Elements;
 
   start = async (): Promise<void> =>
   {
-    this.fetchData();
-    this.elems_taskLists = document.querySelector(".task-list");
+    this.elements.taskLists = document.querySelector(".task-list");
+    this.elements.taskRegisterTitle = document.querySelector(".form__title");
+    this.elements.taskRegister = document.querySelector(".form__submit");
+    this.elements.taskRegister?.addEventListener("click", this.registerTask);
+
+    await this.updateTasksData();
 
     return;
   };
 
 
-  private fetchData = async (): Promise<void> => {
+  private updateTasksData = async (isDisplay: Boolean = true): Promise<void> => {
     // const options: object = {
     //   mode: 'no-cors', // no-cors, *cors, same-origin
     //   method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -39,20 +49,21 @@ export class Application {
 
     this.tasks = await response.json() as Array<Task>;
 
-    this.displayTasks();
-
+    if (isDisplay) this.displayTasks();
   }
-  private displayTasks = (): void => {
 
-    if (this.elems_taskLists == null) return;
+  private displayTasks = (): void =>
+  {
+    if (this.elements.taskLists == null) return;
 
-    this.elems_taskLists.innerHTML = "";
+    this.elements.taskLists.innerHTML = "";
 
     for (let i = 0, len = this.tasks.length; i < len; i++)
     {
-      this.elems_taskLists.appendChild(this.makeTaskDOM(this.tasks[i]));
+      this.elements.taskLists.appendChild(this.makeTaskDOM(this.tasks[i]));
     }
   }
+
   private makeTaskDOM = (task: Task): HTMLElement => {
 
     const elem_task = document.createElement("li");
@@ -60,7 +71,12 @@ export class Application {
     const elem_taskTitle = document.createElement("div");
 
     elem_task.className = `task task--${task.status == TaskStatus.TODO ? 'todo' : 'done'}`;
+    elem_task.setAttribute("data-id", task.id.toString());
+    elem_task.setAttribute("data-status", task.status.toString());
+
     elem_taskBtn.className = "task__btn";
+    elem_taskBtn.addEventListener("click", this.updateTaskStatus);
+
     elem_taskTitle.className = "task__title";
     elem_taskTitle.textContent = task.title;
     elem_taskTitle.setAttribute("data-test", "task-title");
@@ -69,5 +85,66 @@ export class Application {
     elem_task.appendChild(elem_taskTitle);
 
     return elem_task;
+  }
+
+  private registerTask = async (e: any): Promise<void> =>
+  {
+    e.preventDefault();
+
+    const title = this.elements.taskRegisterTitle?.value;
+
+    if (this.elements.taskRegisterTitle == null ||
+      !this.validateTaskTitle(title as string)) return;
+
+    const options: object = {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      // body: `title=${title}`
+      body: JSON.stringify({ title }),
+    };
+
+    const response = await fetch(`${API_URL}/api/tasks/register`, options);
+    if (!response.ok) throw new Error("Network response was not ok.");
+
+    this.updateTasksData();
+  }
+
+  private validateTaskTitle = (title: string): boolean => {
+    title = title.trim();
+    console.log(title);
+    return (
+      title != "" &&
+      !(title.match(/[^A-Za-z0-9]+/))
+    );
+  }
+
+  private updateTaskStatus = async (e: any): Promise<void> =>
+  {
+    e.preventDefault();
+
+    const elem_task = e.target.parentNode;
+    const body: Task = {
+      id: parseInt(elem_task.getAttribute("data-id")),
+      status: parseInt(elem_task.getAttribute("data-status")),
+      title: elem_task.querySelector("[data-test=task-title]").textContent,
+    }
+
+    // Task Status Toggle
+    body.status = body.status == 0 ? 1 : 0;
+
+    const options: object = {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(body),
+    };
+
+    const response = await fetch(`${API_URL}/api/tasks/update`, options);
+    if (!response.ok) throw new Error("Network response was not ok.");
+
+    this.updateTasksData();
   }
 }
